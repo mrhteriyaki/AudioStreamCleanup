e5b57d80a1668ea93f62f420dd562079a543c66d
Mitchell Hayden <m@mrhsystems.com>
2021-02-28 22:37:41 +1100

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnScan = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.txtScanLocation = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnScan
+        '
+        Me.btnScan.Location = New System.Drawing.Point(10, 10)
+        Me.btnScan.Name = "btnScan"
+        Me.btnScan.Size = New System.Drawing.Size(75, 23)
+        Me.btnScan.TabIndex = 0
+        Me.btnScan.Text = "Scan Folder"
+        Me.btnScan.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(417, 16)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(40, 13)
+        Me.lblStatus.TabIndex = 1
+        Me.lblStatus.Text = "Status:"
+        '
+        'txtScanLocation
+        '
+        Me.txtScanLocation.Location = New System.Drawing.Point(91, 13)
+        Me.txtScanLocation.Name = "txtScanLocation"
+        Me.txtScanLocation.Size = New System.Drawing.Size(320, 20)
+        Me.txtScanLocation.TabIndex = 2
+        Me.txtScanLocation.Text = "G:\Videos\Movies\"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1384, 861)
+        Me.Controls.Add(Me.txtScanLocation)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnScan)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnScan As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents txtScanLocation As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,185 @@
+﻿
+Imports Xabe.FFmpeg
+Imports System.Threading
+
+Public Class Form1
+    Dim FileName As String
+    Dim ItemHeightCounter As Integer = 40
+
+    Private Sub btnScan_Click(sender As Object, e As EventArgs) Handles btnScan.Click
+        If Not txtScanLocation.Text.EndsWith("\") Then
+            txtScanLocation.Text = txtScanLocation.Text & "\"
+        End If
+        Dim ScanFunctionThread As New Thread(AddressOf ScanFunction)
+        ScanFunctionThread.Start()
+
+
+    End Sub
+
+
+    Sub AddControlFromThread()
+
+        For Each TXBX In TextBoxList
+            Me.Controls.Add(TXBX)
+        Next
+
+        For Each TXBX In DefaultAudioStream
+            Me.Controls.Add(TXBX)
+        Next
+
+        For Each CMBX In ComboBoxList
+            Me.Controls.Add(CMBX)
+        Next
+    End Sub
+
+
+    Dim TextBoxList As New List(Of TextBox)
+    Dim DefaultAudioStream As New List(Of TextBox)
+    Dim ComboBoxList As New List(Of ComboBox)
+    Private Sub ScanFunction()
+
+
+
+        Dim MediaFileList As New List(Of String)
+        Dim Location As String = "G:\Videos\Movies\A\"
+
+        InvokeControl(lblStatus, Sub(x) x.Text = "Status: Scanning Files")
+        For Each MediaFile In My.Computer.FileSystem.GetFiles(Location, FileIO.SearchOption.SearchAllSubDirectories, "*.mkv")
+            MediaFileList.Add(MediaFile)
+        Next
+
+        For Each MediaFile In My.Computer.FileSystem.GetFiles(Location, FileIO.SearchOption.SearchAllSubDirectories, "*.mp4")
+            MediaFileList.Add(MediaFile)
+        Next
+
+
+        For Each MediaFile In My.Computer.FileSystem.GetFiles(Location, FileIO.SearchOption.SearchAllSubDirectories, "*.avi")
+            MediaFileList.Add(MediaFile)
+        Next
+
+
+        InvokeControl(lblStatus, Sub(x) x.Text = "Status: Scanning Media Info")
+
+
+        FFmpeg.SetExecutablesPath("C:\ffmpeg\bin")
+
+
+        For Each MediaFile In MediaFileList
+
+            FileName = MediaFile
+
+            WaitForTaskComplete = False
+            Dim GetFileInfoTask As New Task(AddressOf GetFileInfo)
+            GetFileInfoTask.Start()
+
+            InvokeControl(lblStatus, Sub(x) x.Text = "Status: Scanning " & FileName)
+            Do Until WaitForTaskComplete = True
+                Threading.Thread.Sleep(100)
+            Loop
+
+
+        Next
+
+        'Generate Combobox for each media file for user to select primary audio track.
+        For Each MFile In MediaList
+            Dim TxtBox As New TextBox
+            TxtBox.Location = New Point(10, ItemHeightCounter)
+            TxtBox.ReadOnly = True
+            TxtBox.Width = 600
+            TxtBox.Text = MFile.filename.Substring(Location.Length)
+            TextBoxList.Add(TxtBox)
+
+            Dim DATxtBox As New TextBox
+            DATxtBox.Location = New Point(TxtBox.Location.X + TxtBox.Width + 10, ItemHeightCounter)
+            DATxtBox.ReadOnly = True
+            DATxtBox.Width = 250
+            DATxtBox.Text = GetAudioStreamDisplayname(MFile.AudioStreams(0))
+            DefaultAudioStream.Add(DATxtBox)
+
+            Dim ComBox As New ComboBox
+            ComBox.Location = New Point(DATxtBox.Location.X + DATxtBox.Width + 10, ItemHeightCounter)
+            ComBox.Width = 250
+
+            For Each AStream In MFile.AudioStreams
+                ComBox.Items.Add(GetAudioStreamDisplayname(AStream))
+            Next
+
+            ComBox.SelectedIndex = 0
+            'Match to first English language if present
+            Dim count As Integer = 0
+            For Each AStream In MFile.AudioStreams
+                If AStream.Language = "eng" Then
+                    ComBox.SelectedIndex = count
+                    Exit For
+                End If
+                count += 1
+            Next
+
+            ComboBoxList.Add(ComBox)
+
+            ItemHeightCounter += 25
+        Next
+
+        Me.Invoke(New MethodInvoker(AddressOf Me.AddControlFromThread))
+
+    End Sub
+
+    Function GetAudioStreamDisplayname(ByVal AStream As AudioStream)
+        Dim DisplayName As String = AStream.Index & ": " & AStream.Language & " - " & AStream.Codec & " - Channels:" & AStream.Channels
+        If Not AStream.Bitrate = 0 Then
+            DisplayName = DisplayName & " - Bitrate:" & AStream.Bitrate
+        End If
+
+
+        Return DisplayName
+    End Function
+
+    Dim WaitForTaskComplete As Boolean
+    Dim MediaList As New List(Of MediaFileStreamInfo)
+
+
+    Async Sub GetFileInfo()
+
+        'Make list of files 
+
+        Dim MediaInfo As IMediaInfo = Await FFmpeg.GetMediaInfo(FileName)
+            If MediaInfo.AudioStreams.Count > 1 Then
+                Dim tmpMFSI As New MediaFileStreamInfo
+                tmpMFSI.filename = FileName
+                For Each AStream In MediaInfo.AudioStreams
+                    tmpMFSI.AudioStreams.Add(AStream)
+                Next
+                MediaList.Add(tmpMFSI)
+            End If
+
+
+            WaitForTaskComplete = True
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Public Sub InvokeControl(Of T As Control)(ByVal Control As T, ByVal Action As Action(Of T))
+        If Control.InvokeRequired Then
+            Try
+                Control.Invoke(New Action(Of T, Action(Of T))(AddressOf InvokeControl), New Object() {Control, Action})
+            Catch ex As Exception
+            End Try
+        Else
+            Action(Control)
+        End If
+    End Sub
+
+
+End Class
+
+
+Public Class MediaFileStreamInfo
+    Public filename As String
+    Public AudioStreams As New List(Of IAudioStream)
+End Class
+
+
