4ac7e0b04902909c83610378081d1c299b7b7d4a
Mitchell Hayden <m@mrhsystems.com>
2021-03-06 18:18:58 +1100

    complete v1

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,6 +25,13 @@ Partial Class Form1
         Me.btnScan = New System.Windows.Forms.Button()
         Me.lblStatus = New System.Windows.Forms.Label()
         Me.txtScanLocation = New System.Windows.Forms.TextBox()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.btnCheckAll = New System.Windows.Forms.Button()
+        Me.btnCheckNone = New System.Windows.Forms.Button()
+        Me.btnPreviousPage = New System.Windows.Forms.Button()
+        Me.btnNextPage = New System.Windows.Forms.Button()
+        Me.lblPage = New System.Windows.Forms.Label()
+        Me.gbxMedia = New System.Windows.Forms.GroupBox()
         Me.SuspendLayout()
         '
         'btnScan
@@ -39,7 +46,7 @@ Partial Class Form1
         'lblStatus
         '
         Me.lblStatus.AutoSize = True
-        Me.lblStatus.Location = New System.Drawing.Point(417, 16)
+        Me.lblStatus.Location = New System.Drawing.Point(7, 36)
         Me.lblStatus.Name = "lblStatus"
         Me.lblStatus.Size = New System.Drawing.Size(40, 13)
         Me.lblStatus.TabIndex = 1
@@ -53,11 +60,81 @@ Partial Class Form1
         Me.txtScanLocation.TabIndex = 2
         Me.txtScanLocation.Text = "G:\Videos\Movies\"
         '
+        'btnProcess
+        '
+        Me.btnProcess.Location = New System.Drawing.Point(1174, 11)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(125, 23)
+        Me.btnProcess.TabIndex = 3
+        Me.btnProcess.Text = "Process Selected"
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'btnCheckAll
+        '
+        Me.btnCheckAll.Location = New System.Drawing.Point(1093, 11)
+        Me.btnCheckAll.Name = "btnCheckAll"
+        Me.btnCheckAll.Size = New System.Drawing.Size(75, 23)
+        Me.btnCheckAll.TabIndex = 4
+        Me.btnCheckAll.Text = "Check All"
+        Me.btnCheckAll.UseVisualStyleBackColor = True
+        '
+        'btnCheckNone
+        '
+        Me.btnCheckNone.Location = New System.Drawing.Point(1012, 11)
+        Me.btnCheckNone.Name = "btnCheckNone"
+        Me.btnCheckNone.Size = New System.Drawing.Size(75, 23)
+        Me.btnCheckNone.TabIndex = 5
+        Me.btnCheckNone.Text = "Check None"
+        Me.btnCheckNone.UseVisualStyleBackColor = True
+        '
+        'btnPreviousPage
+        '
+        Me.btnPreviousPage.Location = New System.Drawing.Point(414, 11)
+        Me.btnPreviousPage.Name = "btnPreviousPage"
+        Me.btnPreviousPage.Size = New System.Drawing.Size(89, 23)
+        Me.btnPreviousPage.TabIndex = 6
+        Me.btnPreviousPage.Text = "Previous Page"
+        Me.btnPreviousPage.UseVisualStyleBackColor = True
+        '
+        'btnNextPage
+        '
+        Me.btnNextPage.Location = New System.Drawing.Point(509, 11)
+        Me.btnNextPage.Name = "btnNextPage"
+        Me.btnNextPage.Size = New System.Drawing.Size(89, 23)
+        Me.btnNextPage.TabIndex = 7
+        Me.btnNextPage.Text = "Next Page"
+        Me.btnNextPage.UseVisualStyleBackColor = True
+        '
+        'lblPage
+        '
+        Me.lblPage.AutoSize = True
+        Me.lblPage.Location = New System.Drawing.Point(604, 15)
+        Me.lblPage.Name = "lblPage"
+        Me.lblPage.Size = New System.Drawing.Size(55, 13)
+        Me.lblPage.TabIndex = 8
+        Me.lblPage.Text = "Page: 0/0"
+        '
+        'gbxMedia
+        '
+        Me.gbxMedia.Location = New System.Drawing.Point(10, 52)
+        Me.gbxMedia.Name = "gbxMedia"
+        Me.gbxMedia.Size = New System.Drawing.Size(1345, 797)
+        Me.gbxMedia.TabIndex = 9
+        Me.gbxMedia.TabStop = False
+        Me.gbxMedia.Text = "Media Files"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1384, 861)
+        Me.Controls.Add(Me.gbxMedia)
+        Me.Controls.Add(Me.lblPage)
+        Me.Controls.Add(Me.btnNextPage)
+        Me.Controls.Add(Me.btnPreviousPage)
+        Me.Controls.Add(Me.btnCheckNone)
+        Me.Controls.Add(Me.btnCheckAll)
+        Me.Controls.Add(Me.btnProcess)
         Me.Controls.Add(Me.txtScanLocation)
         Me.Controls.Add(Me.lblStatus)
         Me.Controls.Add(Me.btnScan)
@@ -71,4 +148,11 @@ Partial Class Form1
     Friend WithEvents btnScan As Button
     Friend WithEvents lblStatus As Label
     Friend WithEvents txtScanLocation As TextBox
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents btnCheckAll As Button
+    Friend WithEvents btnCheckNone As Button
+    Friend WithEvents btnPreviousPage As Button
+    Friend WithEvents btnNextPage As Button
+    Friend WithEvents lblPage As Label
+    Friend WithEvents gbxMedia As GroupBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -4,12 +4,38 @@ Imports System.Threading
 
 Public Class Form1
     Dim FileName As String
-    Dim ItemHeightCounter As Integer = 40
+    Dim ItemHeightCounter As Integer
+    Dim RowCount As Integer = 20
+    Dim PageCount As Integer
+    Dim MaxPageCount As Integer
+    Dim WaitForTaskComplete As Boolean
+    Dim MediaList As List(Of MediaFileStreamInfo)
+
+    Dim TextBoxList As List(Of TextBox)
+    Dim DefaultAudioStream As List(Of TextBox)
+    Dim ComboBoxList As List(Of ComboBox)
+    Dim CheckBoxList As List(Of CheckBox)
+
 
     Private Sub btnScan_Click(sender As Object, e As EventArgs) Handles btnScan.Click
+        btnScan.Enabled = False
+        ItemHeightCounter = 15 'Set Item height start
+
+
+        MaxPageCount = 0
+        PageCount = 1
+        MediaList = New List(Of MediaFileStreamInfo)
+
+        TextBoxList = New List(Of TextBox)
+        DefaultAudioStream = New List(Of TextBox)
+        ComboBoxList = New List(Of ComboBox)
+        CheckBoxList = New List(Of CheckBox)
+
+
         If Not txtScanLocation.Text.EndsWith("\") Then
             txtScanLocation.Text = txtScanLocation.Text & "\"
         End If
+
         Dim ScanFunctionThread As New Thread(AddressOf ScanFunction)
         ScanFunctionThread.Start()
 
@@ -17,31 +43,38 @@ Public Class Form1
     End Sub
 
 
+
     Sub AddControlFromThread()
 
-        For Each TXBX In TextBoxList
-            Me.Controls.Add(TXBX)
-        Next
+        'Calculate Range
+        Dim ControlCounter As Integer = (PageCount * RowCount) - RowCount 'Start Range.
 
-        For Each TXBX In DefaultAudioStream
-            Me.Controls.Add(TXBX)
-        Next
+        Dim CounterLimit As Integer = ControlCounter + RowCount
+        If CounterLimit > MediaList.Count Then
+            CounterLimit = MediaList.Count 'max limit.
+        End If
 
-        For Each CMBX In ComboBoxList
-            Me.Controls.Add(CMBX)
-        Next
+        gbxMedia.Controls.Clear()
+
+        Do Until ControlCounter = CounterLimit
+            gbxMedia.Controls.Add(TextBoxList(ControlCounter))
+            gbxMedia.Controls.Add(DefaultAudioStream(ControlCounter))
+            gbxMedia.Controls.Add(ComboBoxList(ControlCounter))
+            gbxMedia.Controls.Add(CheckBoxList(ControlCounter))
+            ControlCounter += 1
+        Loop
+
     End Sub
 
 
-    Dim TextBoxList As New List(Of TextBox)
-    Dim DefaultAudioStream As New List(Of TextBox)
-    Dim ComboBoxList As New List(Of ComboBox)
+
+
     Private Sub ScanFunction()
 
 
 
         Dim MediaFileList As New List(Of String)
-        Dim Location As String = "G:\Videos\Movies\A\"
+        Dim Location As String = txtScanLocation.Text
 
         InvokeControl(lblStatus, Sub(x) x.Text = "Status: Scanning Files")
         For Each MediaFile In My.Computer.FileSystem.GetFiles(Location, FileIO.SearchOption.SearchAllSubDirectories, "*.mkv")
@@ -80,8 +113,14 @@ Public Class Form1
 
         Next
 
+        InvokeControl(lblStatus, Sub(x) x.Text = "Status: Loading controls.")
+
         'Generate Combobox for each media file for user to select primary audio track.
+
+        Dim InitialHeight As Integer = ItemHeightCounter
+        Dim Counter As Integer = 0
         For Each MFile In MediaList
+
             Dim TxtBox As New TextBox
             TxtBox.Location = New Point(10, ItemHeightCounter)
             TxtBox.ReadOnly = True
@@ -104,25 +143,88 @@ Public Class Form1
                 ComBox.Items.Add(GetAudioStreamDisplayname(AStream))
             Next
 
-            ComBox.SelectedIndex = 0
+            ComBox.SelectedIndex = 0 'Set Default audio track if none auto-matched.
+
+
             'Match to first English language if present
             Dim count As Integer = 0
+
+            Dim channels As Integer = 0
+            Dim bitrate As Integer = 0
+
+
             For Each AStream In MFile.AudioStreams
                 If AStream.Language = "eng" Then
-                    ComBox.SelectedIndex = count
-                    Exit For
+                    If AStream.Bitrate = 0 Then
+                        'Matched to bitstream protocol - DTS / TrueHD
+                        ComBox.SelectedIndex = count
+                        Exit For
+                    End If
+
+                    'if channel count already matches, use highest bitrate.
+                    If AStream.Channels = channels Then
+                        If AStream.Bitrate > bitrate Then
+                            bitrate = AStream.Bitrate
+                            ComBox.SelectedIndex = count
+                        End If
+                    End If
+
+
+                    'Order by highest channel count
+                    If AStream.Channels > channels Then
+                        channels = AStream.Channels
+                        bitrate = AStream.Bitrate
+                        ComBox.SelectedIndex = count
+                    End If
+
                 End If
+
+
                 count += 1
             Next
 
             ComboBoxList.Add(ComBox)
 
+            Dim CheckBox As New CheckBox
+            CheckBox.Name = "chkbox" & MFile.index
+            CheckBox.Text = MFile.index
+            CheckBox.Location = New Point(ComBox.Location.X + ComBox.Width + 10, ItemHeightCounter)
+            CheckBoxList.Add(CheckBox)
+
+            'Increase Line Height
             ItemHeightCounter += 25
+
+
+            'Reset when row count reached.
+            Counter += 1
+            If Counter = RowCount Then
+                ItemHeightCounter = InitialHeight
+                Counter = 0
+            End If
+
+
         Next
+
+        'Calculate how many pages.
+        Dim Remain As Integer
+        Dim Dividevalue As Integer = Math.DivRem(MediaList.Count, RowCount, Remain)
+        If Not Remain = 0 Then
+            Dividevalue += 1
+        End If
+
+        MaxPageCount = Dividevalue
+        Me.Invoke(New MethodInvoker(AddressOf Me.UpdatePageLabel))
 
         Me.Invoke(New MethodInvoker(AddressOf Me.AddControlFromThread))
 
+        InvokeControl(lblStatus, Sub(x) x.Text = "Status: Scanning Complete")
+        InvokeControl(btnScan, Sub(x) x.Enabled = True)
     End Sub
+
+
+
+
+
 
     Function GetAudioStreamDisplayname(ByVal AStream As AudioStream)
         Dim DisplayName As String = AStream.Index & ": " & AStream.Language & " - " & AStream.Codec & " - Channels:" & AStream.Channels
@@ -134,8 +236,7 @@ Public Class Form1
         Return DisplayName
     End Function
 
-    Dim WaitForTaskComplete As Boolean
-    Dim MediaList As New List(Of MediaFileStreamInfo)
+
 
 
     Async Sub GetFileInfo()
@@ -143,22 +244,34 @@ Public Class Form1
         'Make list of files 
 
         Dim MediaInfo As IMediaInfo = Await FFmpeg.GetMediaInfo(FileName)
-            If MediaInfo.AudioStreams.Count > 1 Then
-                Dim tmpMFSI As New MediaFileStreamInfo
-                tmpMFSI.filename = FileName
-                For Each AStream In MediaInfo.AudioStreams
-                    tmpMFSI.AudioStreams.Add(AStream)
-                Next
-                MediaList.Add(tmpMFSI)
-            End If
+        If MediaInfo.AudioStreams.Count > 1 Then
+            Dim tmpMFSI As New MediaFileStreamInfo
+            tmpMFSI.filename = FileName
+            For Each AStream In MediaInfo.AudioStreams
+                tmpMFSI.AudioStreams.Add(AStream)
+            Next
+            tmpMFSI.index = MediaList.Count + 1
+            MediaList.Add(tmpMFSI)
+        End If
 
 
-            WaitForTaskComplete = True
+        WaitForTaskComplete = True
 
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim ValidateFileInfo As IMediaInfo
+    Dim ValidateTaskComplete As Boolean = False
+    Async Sub ValidateFile()
+        'Make list of files 
+        ValidateFileInfo = Await FFmpeg.GetMediaInfo(FileName)
+        ValidateTaskComplete = True
 
+    End Sub
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
@@ -173,12 +286,159 @@ Public Class Form1
         End If
     End Sub
 
+    Private Sub btnCheckNone_Click(sender As Object, e As EventArgs) Handles btnCheckNone.Click
+        For Each chkbox In CheckBoxList
+            chkbox.Checked = False
+        Next
+    End Sub
+
+    Private Sub btnCheckAll_Click(sender As Object, e As EventArgs) Handles btnCheckAll.Click
+        For Each chkbox In CheckBoxList
+            chkbox.Checked = True
+        Next
+    End Sub
+
+    Private Sub btnNextPage_Click(sender As Object, e As EventArgs) Handles btnNextPage.Click
+        If PageCount < MaxPageCount Then
+            PageCount += 1
+            UpdatePageLabel()
+            AddControlFromThread
+        End If
+
+    End Sub
+
+    Private Sub btnPreviousPage_Click(sender As Object, e As EventArgs) Handles btnPreviousPage.Click
+        If PageCount > 1 Then
+            PageCount -= 1
+            UpdatePageLabel()
+            AddControlFromThread()
+        End If
+    End Sub
+
+    Private Sub UpdatePageLabel()
+        lblPage.Text = "Page: " & PageCount & "/" & MaxPageCount
+    End Sub
+
+    Dim ErrorOutput As String
+    Dim StandardOutput As String
+
+    Private Sub ErrorInfo(ByVal sender As Object, ByVal e As DataReceivedEventArgs)
+        ErrorOutput = e.Data & Environment.NewLine & ErrorOutput
+        MsgBox(e.Data)
+    End Sub
+
+    Private Sub OutputInfo(ByVal sender As Object, ByVal e As DataReceivedEventArgs)
+        StandardOutput = e.Data & Environment.NewLine & StandardOutput
+        MsgBox(e.Data)
+    End Sub
+
+
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+        btnProcess.Enabled = False
+        Dim ProcessIndexList As New List(Of Integer)
+        For Each Item In CheckBoxList
+            If Item.Checked = True Then
+                ProcessIndexList.Add(Integer.Parse(Item.Text) - 1)
+            End If
+        Next
+
+        'Process List.
+
+
+        For Each IndexInt In ProcessIndexList
+
+            Dim AudioStreamIndex As Integer = ComboBoxList(IndexInt).SelectedIndex
+
+            Dim tmpfilename As String = ""
+            Dim splitext() As String = MediaList(IndexInt).filename.Split(".")
+            Dim count As Integer = 0
+            For Each splt In splitext
+                count += 1
+                If count = splitext.Length Then
+                    tmpfilename = tmpfilename & "tmp." & splt
+                    Exit For
+                End If
+                tmpfilename = tmpfilename & splt & "."
+
+            Next
+
+
+            lblStatus.Text = "Status: Processing " & MediaList(IndexInt).filename
+
+            'Generate new file
+            'ffmpeg -i <videofile> -map 0:0 -c:v copy -map 0:4 -c:a copy -c:s copy <videofileoutput>
+            Dim ConvertProcess As New Process
+            ConvertProcess.StartInfo.FileName = "C:\ffmpeg\bin\ffmpeg.exe"
+            ConvertProcess.StartInfo.Arguments = "-i " & Chr(34) & MediaList(IndexInt).filename & Chr(34) & " -map 0:0 -c:v copy -map 0:" & MediaList(IndexInt).AudioStreams(AudioStreamIndex).Index & " -c:a copy -c:s copy " & Chr(34) & tmpfilename & Chr(34)
+            ConvertProcess.StartInfo.WorkingDirectory = "C:\ffmpeg\bin"
+
+            'ConvertProcess.StartInfo.CreateNoWindow = True
+            'ConvertProcess.StartInfo.UseShellExecute = False
+            'ConvertProcess.StartInfo.RedirectStandardOutput = True
+            'ConvertProcess.StartInfo.RedirectStandardError = True
+            'AddHandler ConvertProcess.ErrorDataReceived, AddressOf ErrorInfo
+            'AddHandler ConvertProcess.OutputDataReceived, AddressOf OutputInfo
+
+            ErrorOutput = ""
+            StandardOutput = ""
+
+            ConvertProcess.Start()
+
+            ConvertProcess.WaitForExit()
+
+            'Validate file, check audio codec.
+            FileName = tmpfilename
+
+            ValidateTaskComplete = False
+            Dim GetFileInfoTask As New Task(AddressOf ValidateFile)
+            GetFileInfoTask.Start()
+
+            Do Until ValidateTaskComplete = True
+                Threading.Thread.Sleep(100) 'Wait for check to complete
+            Loop
+
+            If ValidateFileInfo.AudioStreams(0).Codec = MediaList(IndexInt).AudioStreams(AudioStreamIndex).Codec Then
+
+                'Basic validation (audio codec readable and matching).
+                'rename existing file to .old ext
+
+                My.Computer.FileSystem.MoveFile(MediaList(IndexInt).filename, MediaList(IndexInt).filename & ".old")
+
+                My.Computer.FileSystem.MoveFile(tmpfilename, MediaList(IndexInt).filename)
+
+                Try
+                    'clear read only flag
+                    System.IO.File.SetAttributes(MediaList(IndexInt).filename & ".old", IO.FileAttributes.Normal)
+                    'delete file
+                    My.Computer.FileSystem.DeleteFile(MediaList(IndexInt).filename & ".old")
+                Catch ex As Exception
+                    MsgBox("error delete file:" & MediaList(IndexInt).filename & ".old " & ex.ToString)
+                End Try
+
+            End If
+
+
+
+
+
+        Next
+
+        'Check Media INFO on file for validation.
+
+
+        'Delete old file and rename new file.
+        lblStatus.Text = "Status: Complete"
+
+        btnProcess.Enabled = True
+    End Sub
+
 
 End Class
 
 
 Public Class MediaFileStreamInfo
     Public filename As String
+    Public index As Integer
     Public AudioStreams As New List(Of IAudioStream)
 End Class
 
